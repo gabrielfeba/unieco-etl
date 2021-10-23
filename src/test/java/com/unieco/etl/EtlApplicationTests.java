e64ddade9e41f7b222821b62282ce78fe6f45fdb
gabriel.de.o.feba <gabriel.de.o.feba@accenture.com>
2021-10-23 13:01:39 -0300

    Start Project

---
+++ src/test/java/com/unieco/etl/EtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unieco.etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
